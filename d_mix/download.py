a160284d20469b5c39092d90361f663811cc1c19
jmpaz <30947643+jmpaz@users.noreply.github.com>
2024-02-26 13:54:11 -0500

    Store the spotdl client globally

--- d_mix/download.py
+++ d_mix/download.py
@@ -4,23 +4,30 @@ from pathlib import Path
 from spotdl import Spotdl
 
 
+def get_spotdl_client(client_id, client_secret):
+    if "spotdl_client" not in globals():
+        globals()["spotdl_client"] = Spotdl(
+            client_id=client_id, client_secret=client_secret, headless=True
+        )
+
+    return globals()["spotdl_client"]
+
+
 def download_tracks(urls, target_dir=".tmp"):
-    # Retrieve secrets from environment variables
     client_id = os.environ.get("SPOTIFY_CLIENT_ID")
     client_secret = os.environ.get("SPOTIFY_CLIENT_SECRET")
 
     if not client_id or not client_secret:
         raise ValueError("Spotify client ID and secret must be provided.")
 
-    # Ensure the target directory exists
+    # Create and enter the target directory
     Path(target_dir).mkdir(parents=True, exist_ok=True)
-
-    # Change the current working directory to the target directory
     os.chdir(target_dir)
 
-    spotdl = Spotdl(client_id=client_id, client_secret=client_secret, headless=True)
+    spotdl = get_spotdl_client(client_id, client_secret)
     songs = spotdl.search(urls)
     tracks = spotdl.download_songs(songs)
+    os.chdir("..")
     return tracks
 
 
